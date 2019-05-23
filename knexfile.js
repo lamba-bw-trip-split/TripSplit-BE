fad8eb67d814bd653a3c43c92dfcd447c4fd8a5d
omar okla <omaro@me.com>
2019-05-22 23:18:32 -0400

    switch prod to postgres

--- knexfile.js
+++ knexfile.js
@@ -21,18 +21,33 @@ module.exports = {
 	},
 
 	production: {
-		client: "postgresql",
-		connection: {
-			database: "my_db",
-			user: "username",
-			password: "password"
+		client: "pg",
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: "./data/migrations"
+		},
+		seeds: {
+			directory: "./data/seeds"
 		},
 		pool: {
-			min: 2,
-			max: 10
+			afterCreate: (conn, done) => {
+				conn.run("PRAGMA foreign_keys = ON", done);
+			}
 		},
+		useNullAsDefault: true
+	},
+
+	testing: {
+		client: "sqlite3",
+		connection: {
+			filename: "./data/test.db3"
+		},
+		useNullAsDefault: true,
 		migrations: {
-			tableName: "knex_migrations"
+			directory: "./data/migrations"
+		},
+		seeds: {
+			directory: "./data/seeds"
 		}
 	}
 };
